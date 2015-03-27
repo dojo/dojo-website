841a91f8c4c3c329dfa310220043774ef3fbabe3
Torrey Rice <rice@sitepen.com>
2015-03-26 20:15:32 -0700

    Initial work for mobile menu
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,31 +1,36 @@
-require([
-		'dojo/ready',
+require(['dojo/ready',
 		'dojo/on',
 		'dojo/dom-class'
 	], function (ready, on, domClass) {
 		 ready(function(){
+			/*
 			var nav = document.querySelector('.nav'),
-				subnav = document.querySelector('.subnav'),
-				fixpoint = document.querySelector('.nav').offsetHeight,
-				scrolled = false;
+			subnav = document.querySelector('.subnav'),
+			fixpoint = document.querySelector('.nav').offsetHeight,
+			scrolled = false;
 
-				function handleScroll() {
-					var docScroll = document.body.scrollTop || document.documentElement.scrollTop;
-					if (scrolled) {
-						if (docScroll < fixpoint) {
-							scrolled = false;
-							domClass.toggle(nav, 'shrink');
-						}
-					}
-					else {
-						if (docScroll >= fixpoint) {
-							domClass.toggle(nav, 'shrink');
-							scrolled = true;
-						}
+			function handleScroll() {
+				var docScroll = document.body.scrollTop || document.documentElement.scrollTop;
+				if (scrolled) {
+					if (docScroll < fixpoint) {
+						scrolled = false;
+						domClass.toggle(nav, 'shrink');
 					}
 				}
+				else {
+					if (docScroll >= fixpoint) {
+						domClass.toggle(nav, 'shrink');
+						scrolled = true;
+					}
+				}
+			}
 
-				on(window, 'scroll', handleScroll);
-				handleScroll();
+			on(window, 'scroll', handleScroll);
+			handleScroll();
+			*/
+
+			on(document.querySelector('.mobileMenuToggle'), 'click', function () {
+				domClass.toggle(document.body, 'menuOpen');
 			});
+		});
 });
