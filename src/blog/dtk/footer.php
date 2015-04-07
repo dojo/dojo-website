21768338ca60794f0ff556257e86ea3d971b9f2f
Torrey Rice <rice@sitepen.com>
2015-04-07 16:44:09 -0700

    more blog styles
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- src/blog/dtk/footer.php
+++ src/blog/dtk/footer.php
@@ -13,11 +13,6 @@
 		</div><!-- #main -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<?php get_sidebar( 'main' ); ?>
-
-			<div class="site-info">
-				<?php do_action( 'twentythirteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 
