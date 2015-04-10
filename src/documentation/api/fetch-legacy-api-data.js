4865648f769d780e6ce9b496cd3683e0a3f38aa6
bitpshr <paulbouchon90@gmail.com>
2015-04-10 13:34:37 -0400

    Create a custom postinstall npm script to pul in and extract old API versions

---
+++ src/documentation/api/fetch-legacy-api-data.js
@@ -0,0 +1,17 @@
+var sys = require('sys'),
+  exec = require('child_process').exec,
+  targz = require('tar.gz'),
+  fsExtra = require('fs-extra'),
+  path = require('path'),
+  config = require('../../../config.js')();å
+
+fsExtra.remove(path.join(config.src, 'documentation/api/_tmp'), function () {
+  exec('git clone https://github.com/lbod/dojo-site-api.git ' + path.join(config.src, 'documentation/api/_tmp'),
+    function (error, stdout, stderr) {
+      new targz().extract(
+        path.join(config.src, 'documentation/api/_tmp/legacyhtml.tar.gz'),
+        path.join(config.src, 'documentation/api'), function () {
+          fsExtra.remove(path.join(config.src, 'documentation/api/_tmp'), function () {}); 
+        });
+  });
+});
