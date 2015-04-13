8d4d7901441981574fbeb429e3575f5f0b38bd67
Torrey Rice <rice@sitepen.com>
2015-04-13 12:46:58 -0700

    Fixing tutorial card grid for mobile
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- src/scripts/tutorials/filter.js
+++ src/scripts/tutorials/filter.js
@@ -8,28 +8,31 @@ require(['dojo/ready',
 		 ready(function(){
 
 			function sizeCards() {
-		 		//Get each card row
-				var rows = query('.row', query('.cardGrid')[0]);
 
-				rows.forEach(function(row, index, nodelist){
-					var cards = query('.card .content', row);
+				if (window.matchMedia("(min-width: 40.063em)").matches) {
 
+			 		//Get each card row
+					var rows = query('.row', query('.cardGrid')[0]);
 
-					if(cards[1]) {
-						cards[0].style.height = 'auto';
-						cards[1].style.height = 'auto';
+					rows.forEach(function(row, index, nodelist){
+						var cards = query('.card .content', row);
 
-						var card0Height = domStyle.get(cards[0], 'height');
-						var card1Height = domStyle.get(cards[1], 'height');
+						if(cards[1]) {
+							cards[0].style.height = 'auto';
+							cards[1].style.height = 'auto';
 
-						// Determine the tallest card and match the other to it.
-						if(card0Height > card1Height) {
-							cards[1].style.height =  card0Height + 'px';
-						} else {
-							cards[0].style.height =  card1Height + 'px';
+							var card0Height = domStyle.get(cards[0], 'height');
+							var card1Height = domStyle.get(cards[1], 'height');
+
+							// Determine the tallest card and match the other to it.
+							if(card0Height > card1Height) {
+								cards[1].style.height =  card0Height + 'px';
+							} else {
+								cards[0].style.height =  card1Height + 'px';
+							}
 						}
-					}
-				});
+					});
+				}
 			}
 
 			sizeCards();
