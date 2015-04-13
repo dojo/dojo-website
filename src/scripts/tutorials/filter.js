7f97e844b840f518972be0344233032a770743e6
Torrey Rice <rice@sitepen.com>
2015-04-13 08:43:32 -0700

    Fixing card resizing
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- src/scripts/tutorials/filter.js
+++ src/scripts/tutorials/filter.js
@@ -7,23 +7,35 @@ require(['dojo/ready',
 	], function (ready, on, domClass, query, domStyle, domAttr) {
 		 ready(function(){
 
-		 	//Get each card row
-			var rows = query('.row', query('.cardGrid')[0]);
+			function sizeCards() {
+		 		//Get each card row
+				var rows = query('.row', query('.cardGrid')[0]);
 
-			rows.forEach(function(row, index, nodelist){
-				var cards = query('.card .content', row);
-				if(cards[1]) {
-					var newHeight;
-					var card0Height = domStyle.get(cards[0], 'height');
-					var card1Height = domStyle.get(cards[1], 'height');
+				rows.forEach(function(row, index, nodelist){
+					var cards = query('.card .content', row);
 
-					// Determine the tallest card and match the other to it.
-					if(card0Height > card1Height) {
-						cards[1].style.height =  card0Height + 'px';
-					} else {
-						cards[0].style.height =  card1Height + 'px';
+
+					if(cards[1]) {
+						cards[0].style.height = 'auto';
+						cards[1].style.height = 'auto';
+
+						var card0Height = domStyle.get(cards[0], 'height');
+						var card1Height = domStyle.get(cards[1], 'height');
+
+						// Determine the tallest card and match the other to it.
+						if(card0Height > card1Height) {
+							cards[1].style.height =  card0Height + 'px';
+						} else {
+							cards[0].style.height =  card1Height + 'px';
+						}
 					}
-				}
+				});
+			}
+
+			sizeCards();
+
+			on(window, "resize", function(){
+				sizeCards();
 			});
 
 		});
