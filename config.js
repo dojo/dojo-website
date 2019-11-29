da7e2b5318ecdfef12ce9a8a54ec359bdf9423c4
dylans <dylan@dojotoolkit.org>
2019-11-29 09:36:05 -0700

    metadata updates for 1.16 release

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = function(grunt){
 
 	config.dojo = {
 		ver: {
-			get major(){ return '1.15' },
+			get major(){ return '1.16' },
 			get minor(){ return '.0'},
 			get full(){ return this.major + this.minor},
 			//get cdn(){ return this.full },
