993b8820db274ccd488a28885c925c9e17b945d4
Dylan Schiemann <dylan@dojotoolkit.org>
2022-03-07 10:55:48 -0700

    metadata updates for 1,17.1 release (#64)

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = function(grunt){
 	config.dojo = {
 		ver: {
 			get major(){ return '1.17' },
-			get minor(){ return '.0'},
+			get minor(){ return '.1'},
 			get full(){ return this.major + this.minor},
 			//get cdn(){ return this.full },
 			get cdn(){ return '1.14.1' },
